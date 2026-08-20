c39edf350cbfe53e75625480e0ba35f703b84ad5
minoru61 <minoru61@users.noreply.github.com>
2026-08-20 10:34:17 +0900

    feat(ec2): allow port 443 (HTTPS) in security group

--- terraform/aws-ec2/main.tf
+++ terraform/aws-ec2/main.tf
@@ -35,6 +35,14 @@ resource "aws_security_group" "macosui_ec2_sg" {
   }
 
   ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "App Port"
     from_port   = 8080
     to_port     = 8080
